8a4ae6ab704c83e68fd6d631f1be88d7a4ac0b12
Kyohei <pvk.ki.ypmr@gmail.com>
2014-06-07 20:17:37 +0900

    アンケート作成

--- apps/frontend/modules/top/templates/topSuccess.php
+++ apps/frontend/modules/top/templates/topSuccess.php
@@ -8,6 +8,67 @@
 	
 	<!-- アンケート -->
 	<article>
+		<FORM METHOD="post" ACTION="">
+			<INPUT TYPE="hidden" NAME="ok_page" VALUE="">
+			<br/>
+			<div>名前</div>
+			<INPUT TYPE="text" NAME="shop name" SIZE="30" MAXLENGTH="40"> 
+			<br/>
+			<div>アドレス</div> 
+			<INPUT TYPE="text" NAME="address" SIZE="30" MAXLENGTH="60">
+			<br/>
+			<br/>
+			<div>楽天市場での店舗運営において、販売促進ツールを使用したことがありますか。</div>
+			<form method="post" action="example.cgi">
+				<select name="q1">
+					<option value="ans0"></option>
+					<option value="ans1">現在使用している。</option>
+					<option value="ans2">以前、使用していたことがある。</option>
+					<option value="ans3">使用したことがない。</option>
+					<option value="ans4">このアンケートで販売促進ツールについて初めて知った。</option>
+				</select>
+			</form>
+			<br/>
+			<div>1つ前の質問で、2または3を選んだ方にお聞きします、何故使用していないまたは使用をやめたのですか？</div> 
+				<input type="checkbox" name="q2" value="ans0">ツールの値段が高い<br/>
+				<input type="checkbox" name="q2" value="ans1">使っても売り上げが上がらなかった（上がらないと思った)<br/>
+				<input type="checkbox" name="q2" value="ans2">店舗のイメージとツールのデザインが合わなかった<br/>
+				<input type="checkbox" name="q2" value="ans3">ツールの使い勝手が悪い<br/>
+				<input type="checkbox" name="q2" value="ans4">その他<br/>
+			<INPUT TYPE="text" NAME="comment" SIZE="200" MAXLENGTH="400">
+			<br/> 
+			<br/>
+			<div>現在、当社では楽天RMSでの入力を簡略化するツールの開発を進めております。そこで楽天RMSでのHTMLの入力に煩雑さを感じたことはありますか。</div>
+			<form method="post" action="example.cgi">
+				<select name="q3">
+					<option value="ans0"></option>
+					<option value="ans1">入力するのがとても煩雑に感じる</option>
+					<option value="ans2">入力するのが煩雑に感じる</option>
+					<option value="ans3">普通</option>
+					<option value="ans4">入力するのは楽だ</option>
+					<option value="ans4">入力するのはとても楽だ</option>
+				</select>
+			</form>
+			<br/>
+			<div>店舗を運営する中で、欲しいと思ったツール、サービスはありますか。</div> 
+				<input type="checkbox" name="q4" value="サンプル">RMSでの入力を簡潔化するツール<br/>
+				<input type="checkbox" name="q4" value="サンプル">サイトの更新日を表示するツール<br/>
+				<input type="checkbox" name="q4" value="サンプル">画像編集を容易にできるツール<br/>
+				<input type="checkbox" name="q4" value="サンプル">バナー広告を表示するツール<br/>
+				<input type="checkbox" name="q4" value="サンプル">ホーム画面や商品画面のレイアウトのテンプレート<br/>
+				<input type="checkbox" name="q4" value="サンプル">出荷予定日を表示するツール<br/>
+				<input type="checkbox" name="q4" value="サンプル">ショップのレビューを表示するツール<br/>
+				<input type="checkbox" name="q4" value="サンプル">その他(自由記述)<br/>
+			<INPUT TYPE="text" NAME="comment" SIZE="200" MAXLENGTH="400">
+			<br/> 
+			<br/>
+			<div>その他、販売促進ツールに関するご意見や当社へのご要望等ございましたらご自由にお書きください。</div> 
+			<INPUT TYPE="text" NAME="comment" SIZE="200" MAXLENGTH="400">
+			<br/> 
+			<br/>
+			<INPUT TYPE="submit" VALUE="送信">
+			<INPUT TYPE="reset" VALUE="クリア">
+		</form>	
 	</article>
 	
 	<!-- サービス内容 -->
