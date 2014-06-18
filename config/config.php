8d37aaebfe395e37fc60ea7dc193de1eacae4fdb
Masa Kato <cmasad743@gmail.com>
2014-06-18 19:25:20 +0900

    commit

--- config/config.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-// symfony directories
-$sf_symfony_lib_dir  = '/Applications/MAMP/bin/php/php5.3.27/lib/php/symfony';
-$sf_symfony_data_dir = '/Applications/MAMP/bin/php/php5.3.27/lib/php/data/symfony';
