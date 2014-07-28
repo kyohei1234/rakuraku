e842c469c456c9ac6ce1b46fa7fdfa322e4c2b54
Masa Kato <cmasad743@gmail.com>
2014-07-28 15:33:51 +0900

    fixed

--- apps/frontend/modules/top/templates/topSuccess.php
+++ apps/frontend/modules/top/templates/topSuccess.php
@@ -9,87 +9,82 @@
   </section>
 
   <section class="top-characteristics">
-      <div class="row">
-        <h2>期間限定！アンケートに答えると更新アプリ無料プレゼント！</h2>
-        <?php echo link_to('<div class="btn">今すぐアンケートに答える</div>', '@survey') ?>
-        <!-- <div class="characteristics-item">
-          <div>
-            <i class="fa fa-desktop"></i>
-          </div>
-          <h4>テキストテキストテキスト</h4>
-          <p>テキストテキストテキスト</p>
+    <div class="row">
+      <h2>期間限定！アンケートに答えると更新アプリ無料プレゼント！</h2>
+      <?php echo link_to('<div class="btn">今すぐアンケートに答える</div>', '@survey') ?>
+      <!-- <div class="characteristics-item">
+        <div>
+          <i class="fa fa-desktop"></i>
         </div>
-        <div class="characteristics-item">
-          <div>
-            <i class="fa fa-smile-o"></i>
-          </div>
-          <h4>テキストテキストテキスト</h4>
-          <p>テキストテキストテキスト</p>
-        </div>
-        <div class="characteristics-item">
-          <div>
-            <i class="fa fa-file-image-o"></i>
-          </div>
-          <h4>テキストテキストテキスト</h4>
-          <p>テキストテキストテキスト</p>
-        </div> -->
+        <h4>テキストテキストテキスト</h4>
+        <p>テキストテキストテキスト</p>
       </div>
-    </section>
-
-    <section class="top-method">
-      <div class="row">
-        <h3 class="method-title">商品データ表作成ツール</h3>
-        <p>RMSを使った面倒な商品情報の入力はもういらない！<br>らくらくにゅうりょくんはRMSでのめんどうなHTML入力をすることなく、商品情報表を出力することができる、販売促進ツールです。</p>
-        <div class="method-list">
-          <div class="method-item">
-            <div class="method-item-inner">
-              <div class="method-text">HTML不要！</div>
-            </div>
-          </div>
-          <div class="method-item">
-            <div class="method-item-inner">
-              <div class="method-text">たったの3分！</div>
-            </div>
-          </div>
-          <div class="method-item">
-            <div class="method-item-inner">
-              <div class="method-text">商品情報表を出力！</div>
-            </div>
-          </div>
+      <div class="characteristics-item">
+        <div>
+          <i class="fa fa-smile-o"></i>
         </div>
-        <?php echo link_to('<div class="btn">もっと詳しく</div>', '@nyuryokun_show') ?>
+        <h4>テキストテキストテキスト</h4>
+        <p>テキストテキストテキスト</p>
       </div>
-    </section>
-
-    <section class="top-curriculum">
-      <div class="row">
-        <h2>Coming Soon</h2>
-        <div class="curriculum-item">
-          <div>
-            <i class="fa fa-calendar"></i>
-          </div>
-          <h4>営業日カレンダー</h4>
-          <p>見やすいカレンダーで営業日をわかりやすく！</p>
+      <div class="characteristics-item">
+        <div>
+          <i class="fa fa-file-image-o"></i>
         </div>
-        <div class="curriculum-item">
-          <div>
-            <i class="fa fa-pencil-square-o"></i>
-          </div>
-          <h4>レビューボックス</h4>
-          <p>お客様に頂いたレビューを簡単に表示！</p>
-        </div>
-        <div class="curriculum-item">
-          <div>
-            <i class="fa fa-sort-numeric-asc"></i>
-          </div>
-          <h4>アイテムランキング</h4>
-          <p>人気のある商品をリストにして順番に！</p>
-        </div>
-      </div>
-<<<<<<< HEAD
-    </section>
-  </article>
-=======
+        <h4>テキストテキストテキスト</h4>
+        <p>テキストテキストテキスト</p>
+      </div> -->
     </div>
   </section>
->>>>>>> c55c906d9237d5e478689cabc58fcd00bc8e77e9
+
+  <section class="top-method">
+    <div class="row">
+      <h3 class="method-title">商品データ表作成ツール</h3>
+      <p>RMSを使った面倒な商品情報の入力はもういらない！<br>らくらくにゅうりょくんはRMSでのめんどうなHTML入力をすることなく、商品情報表を出力することができる、販売促進ツールです。</p>
+      <div class="method-list">
+        <div class="method-item">
+          <div class="method-item-inner">
+            <div class="method-text">HTML不要！</div>
+          </div>
+        </div>
+        <div class="method-item">
+          <div class="method-item-inner">
+            <div class="method-text">たったの3分！</div>
+          </div>
+        </div>
+        <div class="method-item">
+          <div class="method-item-inner">
+            <div class="method-text">商品情報表を出力！</div>
+          </div>
+        </div>
+      </div>
+      <?php echo link_to('<div class="btn">もっと詳しく</div>', '@nyuryokun_show') ?>
+    </div>
+  </section>
+
+  <section class="top-curriculum">
+    <div class="row">
+      <h2>Coming Soon</h2>
+      <div class="curriculum-item">
+        <div>
+          <i class="fa fa-calendar"></i>
+        </div>
+        <h4>営業日カレンダー</h4>
+        <p>見やすいカレンダーで営業日をわかりやすく！</p>
+      </div>
+      <div class="curriculum-item">
+        <div>
+          <i class="fa fa-pencil-square-o"></i>
+        </div>
+        <h4>レビューボックス</h4>
+        <p>お客様に頂いたレビューを簡単に表示！</p>
+      </div>
+      <div class="curriculum-item">
+        <div>
+          <i class="fa fa-sort-numeric-asc"></i>
+        </div>
+        <h4>アイテムランキング</h4>
+        <p>人気のある商品をリストにして順番に！</p>
+      </div>
+    </div>
+  </section>
+</article>
